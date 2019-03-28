27084ba832f8af9cdf55830144991eb95049ed29
John Hill <john.m.hill91@gmail.com>
2019-03-28 01:20:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@ def key_for_min_value(name_hash)
   minVal=nil 
   minKey=nil
 name_hash.each do |key, value|
- minKey=key if minKey=nil
- minVal=value if minVal=nil
+ minKey=key if minKey==nil
+ minVal=value if minVal==nil
  if value < minVal 
    minVal= value
    minKey= key
